c9e59c6b7c8e3306b759ff93c7b5a8b6b6fda0fe
Dharani <dharanik22@apple.com>
2023-12-03 12:07:42 +0530

    second commit

--- Program9_Dict.py
+++ Program9_Dict.py
@@ -39,5 +39,57 @@ print(f'The values in dictionary d after updating Key 200 is:')
 for x in d1:
     print(f"{x} -> {d1[x]}")
 
+print(f'dictionary before deleting a key:{d1}')
+del d1[100]  # deleting a key
+print(f'dictionary after deleting a key:{d1}')
+
+try:
+    del d1[500]  # if the key doesn't exist we get KeyError
+except KeyError:
+    print(f"The key 500 doesn't exist in {d1}")
+
+d1.clear()  # to delete all the elements
+print(f'dictionary after calling clear method: {d1}')
+
+list1 = [10, 20, 30]
+d = {100: list1, 200: {'Dharani', 10, 20, 30}}  # we can have multiple values for a key
+print(f'dictionary where key contains multiple values: {d}')
+
+d = dict([(100, 'dharani'), (200, 'Ravi'), (300, 'Shiva')])  # we can pass tuple of tuples or set of tuples as well
+print(f'dictionary created from list of tuples: {d}')
+print(f'The length of the dictionary is: {len(d)}')
+
+print(f'The value of the Key 100 using get method is: {d.get(100)}')
+print(f'The value of the Key 500 using get method is: {d.get(500)}')  # this returns None
+print(f'The value of the Key 500 using get method is: {d.get(500, "Not found")}')  # we can pass default value as well
+
+print(f'Remove and return the value associated with the key: {d.pop(100)}')
+
+try:
+    print(f'Remove and return the value associated with the key: {d.pop(1000)}')
+except KeyError:
+    print(f"The key 1000 doesn't exist")
+
+print(f'Removing some random key-value pair from dictionary: {d.popitem()}')
+print(f'Removing some random key-value pair from dictionary: {d.popitem()}')
+
+# print(f'Removing some random key-value pair from dictionary: {d.popitem()}')
+# KeyError: 'popitem(): dictionary is empty'
+
+print(f'The values in dictionary are: {d}')
+
+d = dict([(100, 'dharani'), (200, 'Ravi'), (300, 'Shiva')])
+print(f'The keys in dictionary are: {d.keys()}')
+print(f'The values in dictionary are: {d.values()}')
+
+for k, v in d.items():
+    print(f'{k} -> {v}')
+
+d1 = d.copy()
+print(f'The variable d1 created from copy method is: {d1}')
+
+d.setdefault(400, 'Kavya')  # If the key doesn't exist adds the key to the dictionary
+d.setdefault(100, 'Sunny')  # since key 100 is already present it doesn't override the old value
+print(f'The dictionary d = {d}')
 
 
