82e85d80630bb97ce02c29b0ae81b8576a0bcf9d
Dharani <dharanik22@apple.com>
2023-12-10 23:55:57 +0530

    logging

---
+++ Program21_Logging.py
@@ -0,0 +1,64 @@
+"""
+The below program explains about logging module in Python
+Log files help us in debugging if something bad happens
+Log files also provides statistics of our application
+To implement logging Python contains one in-built module called logging
+
+Different logging levels in Python.
+
+    1. CRITICAL => 50
+    2. ERROR => 40
+    3. WARNING => 30
+    4. INFO => 20
+    5. DEBUG => 10
+    6. NOTSET => 0
+
+If we don't set any log level by default we will get WARNING and above levels added to our program
+To implement logging we need to have the file which stores the log info and the level it needs to store
+We can implement logging by using basicConfig() function of logging module
+logging.basicConfig(filename='log.txt',level=logging.INFO)
+
+After creating log file we can use the below methods to write messages to the log file created
+logging.debug(message)
+logging.info(message)
+logging.warning(message)
+logging.error(message)
+logging.critical(message)
+
+The most common way of debugging is using print statement but after fixing the problem we need to make sure
+we remove the statements which were added as part of debugging.
+
+Instead of using print statement for debugging we can use assert statement.
+There are two types of assert statements in Python.
+
+    1. Simple version
+    2. Very simple version(Augmented version)
+"""
+
+import logging
+logging.basicConfig(filename='log.txt',level=logging.DEBUG)  # It will by default create log.txt file
+# If the file is already available it will append the data to the file
+
+print('Python logging demo')
+
+logging.debug("This is debug message")  # this methods will write data to the log file we specify
+logging.info("This is info message")
+logging.critical("This is critical message")
+logging.error("This is error message")
+logging.warning("This is warning message")
+
+print('A new request came to use our application')
+logging.info('A new request came to use our application')
+try:
+    x = int(input('Enter the first number:'))
+    y = int(input('Enter the second number:'))
+    print(x/y)
+except ZeroDivisionError as msg:
+    print('cannot divide with zero')
+    logging.exception(msg)
+except ValueError as msg:
+    print('Please pass only int values')
+    logging.exception(msg)
+logging.info('Request processing completed')
+
+
